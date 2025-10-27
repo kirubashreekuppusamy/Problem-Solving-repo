167ae9ac8fcf7a117b78d34c3480f4a29a6ece08
kirubashreekuppusamy <146115167+kirubashreekuppusamy@users.noreply.github.com>
2025-10-27 17:51:50 +0530

    Add files via upload

---
+++ Sandbox-pattern.py
@@ -0,0 +1,20 @@
+n=int(input())
+for i in range(n-1,0,-1):
+    for s in range(n-i-1):
+        print(" ",end=" ")
+    for str in range(2*i+1):
+        print("*",end=" ")
+    print()
+for i in range(1,n+1):
+    for s in range(n-i):
+        print(" ",end=" ")
+    for str in range(2*i-1):
+        print("*",end=" ")
+    print()
+
+
+    
+
+
+
+    
